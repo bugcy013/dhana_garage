0fb42c72defeecac8d2711a99387007edd6ecc72
Dhanasekaran Anbalagan <bugcy013@gmail.com>
2016-03-20 15:57:00 +0530

    Initial commit

--- routes.py
+++ routes.py
@@ -4,15 +4,15 @@ from flask import Flask, url_for, request, render_template
 from app import *
 import redis
 
+# connect to redis data store
+r = redis.StrictRedis(host='localhost', port=6379, db=0, charset='utf-8', decode_responses=True)
+# alternative way connect redis
+# r = redis.StrictRedis()
+# r = redis.StrictRedis('localhost',6379,0)
+
 # server/
 @app.route('/')
 def hello():
-    # connect to redis data store
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
-    # alternative way connect redis
-    # r = redis.StrictRedis()
-    # r = redis.StrictRedis('localhost',6379,0)
-
 
     createLink = "<a href='" + url_for('create') + "'>Create a Question</a>"
     return """<html>
@@ -37,10 +37,17 @@ def create():
         answer = request.form['answer']
         question = request.form['question']
 
-        # templatefilename <parameter> <variable>
-        return render_template('CreatedQuestion.html', question = question)
-
         # Store data in data store
+        # key name will be whatever title they typed in : Question
+        # e.g. music:question countries:question
+        # e.g. music:answer countries:answer
+        # templatefilename <parameter> <variable>
+
+        r.set(title +':question', question)
+        r.set(title +':answer', answer)
+
+        return render_template('CreatedQuestion.html', question=question)
+
     else:
         return "<h2>Invalid request</h2>"
 
@@ -49,20 +56,17 @@ def create():
 @app.route('/question/<title>', methods=['GET', 'POST'])
 def question(title):
     if request.method  == 'GET':
-        question = 'Question here'
         # read question from date store
+        question = r.get(title+':question')
         return render_template('AnswerQuestion.html', question=question)
 
     elif request.method == 'POST':
         submittedAnswer = request.form['submittedAnswer']
-
-
         # Read answer from data store
-
-        answer = 'Answer'
+        answer = r.get(title+':answer')
 
         if submittedAnswer == answer:
-            return  render_template('Correct.html')
+            return render_template('Correct.html')
 
         else:
             return render_template('InCorrect.html', submittedAnswer=submittedAnswer, answer=answer)
